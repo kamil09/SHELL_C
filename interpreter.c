270f1e4053e7211a56554884802ab47637566d92
Kamil <kpiotrowski@comea.pl>
2015-12-20 23:47:29 +0100

    Początki wstrzymywania procesów, nie działa jeszcze :/

--- interpreter.c
+++ interpreter.c
@@ -16,16 +16,35 @@ int iloscLini = 0;		//Ilość lini
 int iloscRownoleglych = 0;
 int argc=0;			//ILOŚC ARGUMENTÓW
 char *argv[200];		//ARGUMENTY
-//int wTle=0;
 int terminal=1;
+int fdPID=-1;
+pid_t ppid=-1;
 
+/**
+ * Funcka do ubsługi CRTL+C
+*/
+void obsluga_CTRL_C(){
+}
 /**
  * Funkcja do obslugi sygnału zakończenia procesu
 */
 void obsluga(int signo){
 	int s;
-	//pid_t pidP = wait(&s);
-	//if(terminal==1) printf("\nProces zakończony. PID: %d Status: %d\n",pidP, s>>8);
+	pid_t pidP = wait(&s);
+	if(terminal==1 && pidP>0) {
+		printf("\nProces zakończony. PID: %d Status: %d\n",pidP, s>>8);
+	//	printf("Wprowadz komendę >>> ");
+		fflush(stdout);
+	}
+}
+void obsluga_CTRL_Z(int signo){
+	if( (fdPID > 0)  && ( fdPID!=ppid ) ){
+		kill( fdPID,SIGSTOP );
+		kill( fdPID,SIGTSTP );
+		printf("Zatrzymany PID: %d\n",fdPID);
+		fdPID=-1;
+		fflush(stdout);
+	}
 }
 /**
  * Funkcja która dzieli linię na kilka mniejszych wykonywanych sekwencyjnie
@@ -106,10 +125,6 @@ int zamienLinie(char *lineT){
 				*(lineT+k)=0;
 				if(*(lineT+(k+1))!=0 ) {
 					argv[argc]=lineT+(k+1);
-					//if(!strcmp(argv[argc],"&")) { 
-					//	wTle=1;
-					//	argv[argc]=0;
-					//}
 					argc++;
 				}
 			}
@@ -126,11 +141,7 @@ int zamienLinie(char *lineT){
  * Funcka która wykonuje pojedyńcze polecenie
 */
 void wykonajPolecenie(int argcT, char *argvT[100]){
-	int err;
-	int i=0;
-	int pid;
-	int wTle=0;
-	int stat=0;
+	int err, i=0, pid, wTle=0, stat=0;
 	int zapis = 0;		// 0 - normalnie ; 1 - zapis < ; 2 - dopisywanie >>
 	int odczyt =0;
 	char *inFILE;
@@ -172,8 +183,16 @@ void wykonajPolecenie(int argcT, char *argvT[100]){
 	}
 
 	
-	
+	signal(SIGCHLD, obsluga);	
+	signal(SIGTSTP, obsluga_CTRL_Z);
+	signal(SIGSTOP, obsluga_CTRL_Z);
+//	signal(SIGUSR1, obsluga_CTRL_C);
+
 	if((pid=fork())==0){
+	//	signal(SIGSTOP, obsluga_CTRL_Z);
+	//	signal(SIGTSTP, obsluga_CTRL_Z);
+		
+		if( wTle == 0) fdPID = pid;
 		if(zapis>0){
 			close(1);
 			if(zapis==1) {
@@ -196,17 +215,16 @@ void wykonajPolecenie(int argcT, char *argvT[100]){
 		else exit(0);
 	}
 	else{
-		signal(SIGCHLD, obsluga);
 		if( wTle == 0) {
-		//	waitpid(pid,&stat,0);
+			fdPID = pid;
+			waitpid(pid, &stat,0);
+			if(terminal==1) {
+				printf("\nProces zakończony. PID: %d Status: %d\n",pid, stat>>8);
+				fflush(stdout);
+			}
 		}
 		else printf("w tle [PID: %d]\n", pid);
 	}
-}
-/**
- * Funcka do ubsługi CRTL+C
-*/
-void obsluga_CTRL_C(){
 }
 /**
  * Główna funkcja programu
@@ -217,28 +235,31 @@ int main(){
 	int status=0;
 	char *line;
 	pid_t pidT;
-	if(isatty(fileno(stdin))!=1 ) terminal=0;					//WCZYTANA LINIA
+	if(isatty(0)!=1 ) terminal=0;					//WCZYTANA LINIA
 	
+	ppid=getpid();
+
 	while(1){
-		//signal(SIGINT,obsluga_CTRL_C);					//Sygnał przerwania
-		
+	//	signal(SIGINT,obsluga_CTRL_C);					//Sygnał przerwania	
+	//	signal(SIGTSTP, obsluga_CTRL_C);
+		signal(SIGTSTP, obsluga_CTRL_Z);
+		signal(SIGSTOP, obsluga_CTRL_Z);
+	
 		if(terminal==1) line=readline("Wprowadz komendę >>> ");	//Wczytanie lini
 		else line=readline("");	
 		if( (line != NULL) && (*line)){
 			add_history(line);						//DODAJE LINIE DO HISTORII
 			podzielLinie(line);						//ROZBIJA NA LINIE SEKWENCYJNE
 			for(i = 0; i < iloscLini ; i++){
-			//	printf("%s\n",wszystkieLinie[i]);
 				podzielLinieWspolbierzne(wszystkieLinie[i]);		//ROZBIJA NA LINIE WSPOLBIERZNE
-			//	printf("%d\n",iloscRownoleglych);
 				for(k = 0 ; k<iloscRownoleglych; k++ ){	
 					if(!strcmp(line,"exit")) exit(0);
 					zamienLinie(komendyRownolegle[k]);				//ZAMIENIA LINIE NA KOMENDY I PARAMETRY W TABLICY
-					if(argv[0]!=0) wykonajPolecenie(argc,argv);			//WYKONUJE POLECENIE
+					if( (argv[0]!=0) && (argv[0][0]>30 ) ) wykonajPolecenie(argc,argv);			//WYKONUJE POLECENIE
 				}
-				while ((pidT = wait(&status)) > 0){
-					if(terminal==1) printf("\nProces zakończony. PID: %d Status: %d\n",pidT, status>>8);				
-				};
+				//while ((pidT = wait(&status)) > 0){
+				//	if(terminal==1) printf("\nProces zakończony. PID: %d Status: %d\n",pidT, status>>8);				
+				//};
 				
 			}
 		}
